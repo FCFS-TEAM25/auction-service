d88bce2624828f9de19b85f8d64e145779ec319c
mummumm <01088287493@daum.net>
2025-04-14 15:21:10 +0900

    ✨ feat [#8] : 경매 상품 조회 api 구현 완료 - 경매 상품 조회 api 관련 파일 추가 및 구현 - 경매 상품 조회 HttpTest 작성

--- src/main/java/com/sparta/limited/auction_service/Auction_product/domain/repository/AuctionProductRepository.java
+++ src/main/java/com/sparta/limited/auction_service/Auction_product/domain/repository/AuctionProductRepository.java
@@ -1,8 +1,13 @@
 package com.sparta.limited.auction_service.Auction_product.domain.repository;
 
 import com.sparta.limited.auction_service.Auction_product.domain.model.AuctionProduct;
+import com.sparta.limited.auction_service.auction.domain.model.Auction;
+import java.util.UUID;
 
 public interface AuctionProductRepository {
 
     void save(AuctionProduct auctionProduct);
+
+    AuctionProduct findById(UUID id);
+
 }
--- src/main/java/com/sparta/limited/auction_service/Auction_product/presentation/controller/AuctionProductController.java
+++ src/main/java/com/sparta/limited/auction_service/Auction_product/presentation/controller/AuctionProductController.java
@@ -3,10 +3,13 @@ package com.sparta.limited.auction_service.Auction_product.presentation.controll
 import com.sparta.limited.auction_service.Auction_product.application.dto.request.AuctionProductCreateRequest;
 import com.sparta.limited.auction_service.Auction_product.application.dto.response.AuctionProductCreateResponse;
 import com.sparta.limited.auction_service.Auction_product.application.service.AuctionProductService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +27,12 @@ public class AuctionProductController {
         @RequestBody AuctionProductCreateRequest request) {
         AuctionProductCreateResponse response = auctionProductService.createAuctionProduct(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<AuctionProductCreateResponse> getAuctionProduct(
+        @PathVariable("id") UUID id) {
+        AuctionProductCreateResponse response = auctionProductService.getAuctionProduct(id);
+        return ResponseEntity.ok(response);
     }
 }
