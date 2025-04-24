0c7fab9a3c24ad6e1de59e607233b933beff4c3a
mummumm <01088287493@daum.net>
2025-04-24 14:31:55 +0900

    ♻️ refactor [#36] : FeignClient url 제거

--- src/main/java/com/sparta/limited/auction_service/auction_product/infrastructure/client/ProductClient.java
+++ src/main/java/com/sparta/limited/auction_service/auction_product/infrastructure/client/ProductClient.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://localhost:19093/api/v1/internal/products")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
-    @GetMapping("/{productId}")
+    @GetMapping("/api/v1/internal/products/{productId}")
     ProductReadResponse getProduct(@PathVariable UUID productId);
 }
